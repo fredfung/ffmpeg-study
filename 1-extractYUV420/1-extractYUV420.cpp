c10e7f481c2ce90904e56252e59f966e30ddc858
Feng FeiFei <duoduo1097@gmail.com>
2019-11-23 15:25:28 +0800

    Initial commit

---
+++ 1-extractYUV420/1-extractYUV420.cpp
@@ -0,0 +1,16 @@
+﻿// 1-extractYUV420.cpp : Defines the entry point for the application.
+//
+
+#include "1-extractYUV420.h"
+
+extern "C" {
+#include "libavcodec/avcodec.h"
+}
+
+using namespace std;
+
+int main()
+{
+	cout << avcodec_configuration() << endl;
+	return 0;
+}
